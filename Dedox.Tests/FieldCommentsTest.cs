bbd2d2cf4de82295b8fa0b64e4829b98c7379a69
Einar W. Høst <einarwh@gmail.com>
2015-02-26 17:58:48 +0100

    Field. Indexer ok for StyleCop.

---
+++ Dedox.Tests/FieldCommentsTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dedox.Tests
+{
+    [TestClass]
+    public class FieldCommentsTest : CommentsTest
+    {
+        [TestMethod]
+        public void StripsBasicPattern()
+        {
+            const string text = @"
+public class Cat 
+{
+   /// <summary>
+   /// The lol wut.
+   /// </summary>
+   public string LolWut; 
+}
+";
+            const string expected = @"
+public class Cat 
+{
+   public string LolWut; 
+}
+";
+            VerifyStrip(text, expected);
+        }
+    }
+}
--- Dedox/GeneratedIndexerCommentsChecker.cs
+++ Dedox/GeneratedIndexerCommentsChecker.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 using Roslyn.Compilers.CSharp;
 
@@ -32,9 +33,102 @@ namespace Dedox
                 return GetPredictedIndexerValueText();
             }
 
+            if ("param".Equals(tag))
+            {
+                return ExpectedCommentForParamTag(startTag);
+            }
+
+
+            if ("returns".Equals(tag))
+            {
+                return GetPredictedIndexerReturnsText();
+            }
+
             Info("Unexpected tag {0} in property comment.", tag);
 
             return new List<Func<string>>();
+        }
+
+        private List<Func<string>> ExpectedCommentForParamTag(XmlElementStartTagSyntax startTag)
+        {
+            string paramNameFromAttribute = "";
+            foreach (var a in startTag.Attributes)
+            {
+                var attrName = a.Name.LocalName.ValueText;
+                var attrValue = string.Join("", a.TextTokens.Select(t => t.ValueText));
+                if ("name".Equals(attrName))
+                {
+                    paramNameFromAttribute = attrValue;
+                }
+                else
+                {
+                    // Unexpected attribute.
+                    return new List<Func<string>>();
+                }
+            }
+
+            // Sanity check for attribute value.
+            var paramExists =
+                It.ParameterList.Parameters.Any(
+                    ps =>
+                    string.Equals(
+                        ps.Identifier.ValueText,
+                        paramNameFromAttribute,
+                        StringComparison.InvariantCultureIgnoreCase));
+
+            if (!paramExists)
+            {
+                Info("! The indexer does not have a parameter named {0}.", paramNameFromAttribute);
+            }
+
+            var decomposedParamName = StyleCopDecompose(paramNameFromAttribute);
+
+            return new List<Func<string>>
+                       {
+                           () => string.Format("The {0}.", decomposedParamName),
+                           () => string.Format("The {0}.", paramNameFromAttribute)
+                       };
+        }
+
+        private List<Func<string>> GetPredictedIndexerReturnsText()
+        {
+            var returnTypeName = GetTypeNameForReturnsComment();
+            if (returnTypeName == null)
+            {
+                Info("Unknown content in return tag.");
+                return new List<Func<string>>();
+            }
+
+            return new List<Func<string>> { () => string.Format("The <see cref=\"{0}\"/>.", returnTypeName) };
+        }
+
+        private string GetTypeNameForReturnsComment()
+        {   
+            var ret = It.Type;
+
+            if (ret.Kind == SyntaxKind.PredefinedType)
+            {
+                // This includes 'void'. It makes little sense to document 'void'. Should detect this case.
+                // At the same time, there will be a mismatch downstream.
+                var predefType = (PredefinedTypeSyntax)ret;
+                return predefType.Keyword.ValueText;
+            }
+
+            if (ret.Kind == SyntaxKind.IdentifierName)
+            {
+                // This includes custom types.
+                var identType = (IdentifierNameSyntax)ret;
+                return identType.Identifier.ValueText;
+            }
+
+            if (ret.Kind == SyntaxKind.GenericName)
+            {
+                var genericName = (GenericNameSyntax)ret;
+                return genericName.Identifier.ValueText;
+            }
+
+            Info("Unknown return type kind: " + ret.Kind);
+            return null;
         }
 
         private List<Func<string>> GetPredictedIndexerValueText()
@@ -44,7 +138,10 @@ namespace Dedox
 
         private List<Func<string>> GetPredictedIndexerSummaryText()
         {
-            return new List<Func<string>>();
+            return new List<Func<string>>
+                       {
+                           () => string.Format("The this.")
+                       };
         }
     }
 }
